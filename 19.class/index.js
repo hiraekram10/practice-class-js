414c15dde97f0cae12ca3a041e11b0c4696500a5
hiraekram10 <heraiqram123@gmail.com>
2023-12-13 21:55:13 +0500

    done

---
+++ 19.class/index.js
@@ -0,0 +1,60 @@
+// let abc = 'hello'
+
+// var abc = "rida"
+// console.log(abc);  //Uncaught SyntaxError: Identifier 'abc' has already been declared
+
+
+
+// function foo (){
+//     var num = 8
+// }
+
+// console.log(num); //num is not defined
+
+// if(true){
+//     // var abc = "hira"
+//     // let abc = "haya"   //abc is not defined
+// }
+
+// console.log(abc);
+
+//perofrmance good for just use no updates
+// const data = "hira"
+// data= "frha"
+// console.log(data);  //error
+
+
+
+// function restPara(a = "not provided", ...rest){
+//     console.log(a,rest);
+// }
+
+
+// function restPara( ...rest){
+//     console.log(rest);
+// }
+// restPara() //not provided
+// restPara("haya","rid@gmail.com", 02437887 ,"female" )
+
+
+// spread operator
+// let fruits = ["orange","mango","apple"]
+// let vegetables = ["potato","onion","carrot",{greet:'hi'}]
+
+
+// let join = [...fruits,...vegetables]
+// console.log(join);
+
+
+// let obj = {
+//     fName :'hira',
+//     lName:'ekram'
+// }
+// let objects = {
+//     email:'hira@gmail.com'
+// }
+// let merge = {...obj,...objects}
+// console.log(merge);
+
+
+// working of spread operator
