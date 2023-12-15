326acc57c5c60e5730f0233ea078ef03b0302a28
hiraekram10 <heraiqram123@gmail.com>
2023-12-15 18:29:10 +0500

    arrow function done

---
+++ 20.class/index.js
@@ -0,0 +1,44 @@
+    
+    //function declearartion 
+
+    // function abc (){
+    //     console.log("hello");
+    // }
+    // abc()
+
+
+    // function expression
+
+    // let foo = function (){
+    //     console.log("hi");
+    // }
+    // foo()
+
+
+     //(let foo;)  hoisting  
+
+    // foo()   //error :Cannot access 'foo' before initialization
+    
+    // let foo = function (){     // hoisting applied
+    //     console.log("hi");
+    // }
+
+
+    // arrow function fat arrow(expression function)
+
+    // let foo = ()=>{
+    //     console.log("hi");
+    // }
+
+    // foo()
+
+
+    //  let foo = a =>{
+    //     return(a+ 7);       ||    let foo = a => a+ 7
+    // }
+
+    // foo(10)
+
+    
+    
+
