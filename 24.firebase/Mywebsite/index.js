8953c335a1b64a9192e3f6817746cdf86903c6c9
hiraekram10 <heraiqram123@gmail.com>
2023-12-27 18:47:37 +0500

    started firebase

---
+++ 24.firebase/Mywebsite/index.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCufpykJkjnwye2aNL8l6PLSv3PG_3Z4BE",
+    authDomain: "learningfb-bbcc5.firebaseapp.com",
+    projectId: "learningfb-bbcc5",
+    storageBucket: "learningfb-bbcc5.appspot.com",
+    messagingSenderId: "351548021683",
+    appId: "1:351548021683:web:003c8c1766da0cd0b309c6",
+    measurementId: "G-TYJC9QGNC8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+
+
+
+
+let register = ()=>{
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+
+    console.log('test',email.value,password.value)
+}
+// window.register = register;
+
+let registerBtn = document.getElementById('registerBtn');
+registerBtn.addEventListener('click',register)
